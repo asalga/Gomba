80a0d02eb51a2ccd595b0006a3c469e9558e4325
Andor Salga <andorsalga@gmail.com>
2014-11-10 11:16:59 -0500

    updated build.

--- build/build.js
+++ build/build.js
@@ -333,9 +333,9 @@ class AtlasParserXML implements AtlasParser {
     return images;
   }
 }
-////////////////////////
-// WrapAroundComponent
-////////////////////////
+/////////////////////////
+// BoundingBoxComponent
+/////////////////////////
 class BoundingBoxComponent extends Component {
 
   float x, y, w, h;
@@ -343,6 +343,7 @@ class BoundingBoxComponent extends Component {
   int mask;
   int type;
   HashMap<String, GameObject> colliders;
+  boolean collidable;
 
   BoundingBoxComponent() {
     super();
@@ -352,6 +353,7 @@ class BoundingBoxComponent extends Component {
     xOffest = yOffset = 0;
     mask = 0;
     colliders = new HashMap<String, GameObject>();
+    collidable = true;
   }
 
   void awake() {
@@ -381,7 +383,14 @@ class BoundingBoxComponent extends Component {
       pushStyle();
       strokeWeight(1);
       noFill();
-      stroke(255, 0, 0);
+      
+      if(isCollisable()){
+        stroke(255, 0, 0);
+      }
+      else{
+       stroke(255, 255, 255); 
+      }
+
       rect(x, -y + yOffset*2, w, h);
       popStyle();
     }
@@ -392,6 +401,14 @@ class BoundingBoxComponent extends Component {
   }
 
   void onCollision(GameObject other) {
+  }
+
+  void setEnableCollisions(boolean b){
+    collidable = b;
+  }
+
+  boolean isCollisable(){
+    return collidable;
   }
 
   void onCollisionEnter(GameObject other) {
@@ -407,7 +424,12 @@ class BoundingBoxComponent extends Component {
 /////////////////////////////
 class BrickControllerComponent extends StructureControllerComponent{
 	
-	float y;
+	// Properties
+	float heightBounce;
+	float speed;
+
+
+	float yPos;
 	boolean bouncing;
 	float original;
 	BoundingBoxComponent bounds;
@@ -415,8 +437,12 @@ class BrickControllerComponent extends StructureControllerComponent{
 
 	BrickControllerComponent(){
 		super();
-		y = 0;
+		yPos = 0;
 		bouncing = false;
+
+		// Properties
+		heightBounce = 8;
+		speed = 10;
 	}
 
 	void awake(){
@@ -447,22 +473,19 @@ class BrickControllerComponent extends StructureControllerComponent{
 
 	void update(float dt){
 		if(bouncing == true){
-			y += dt * 15.0;
+			yPos += dt * speed;
 
 			if(animation != null){
-				// TODO: fix
-				float ytemp = (15 * sin(y));
-				animation.setPosition(0, ytemp);
+				animation.setPosition(0, heightBounce * sin(yPos));
 			}
 		}
-		if(y >= PI){
-			y = 0;
+		if(yPos >= PI){
+			yPos = 0;
 			bouncing = false;
 			animation.setPosition(0, 0);
 		}
 	}
 }
-
 //////////////////////////
 // BoundingBoxXComponent
 //////////////////////////
@@ -580,7 +603,6 @@ class BoundingBoxYComponent extends BoundingBoxComponent {
 
     // STRUCTURE
     else if(other.hasTag("structure")){
-      mario.hitStructureY(other);
 
       PhysicsComponent phy = (PhysicsComponent)gameObject.getComponent("PhysicsComponent");
 
@@ -595,8 +617,11 @@ class BoundingBoxYComponent extends BoundingBoxComponent {
 
         mario._isJumping = false;
       }
+      else{
+        mario.hitStructureY(other);
+      }
     }
-  } 
+  }
 }
 ////////////////////
 // CameraComponent
@@ -761,6 +786,10 @@ class CollisionManager {
             BoundingBoxComponent bb2 = (BoundingBoxComponent)bbList2.get(bbList2Index);
 
             if(bb1 == null || bb2 == null){
+              continue;
+            }
+
+            if(bb1.isCollisable() == false || bb2.isCollisable() == false){
               continue;
             }
 
@@ -1633,12 +1662,14 @@ final int KEY_F12 = 122;
 //////////////////////////
 class MarioControllerComponent extends Component {
 
-  final float walkForce = 20; //550
-  final float jumpForce = 650; //350
-  //BoundingBoxComponent boundingBox;
+  final float walkForce = 20;
+  final float jumpForce = 650;
 
   PhysicsComponent physics;
   AnimationComponent animation;
+  float jumpTimer = 0;
+
+  BoundingBoxComponent boundingBox;
 
   boolean canWalk;
 
@@ -1663,11 +1694,21 @@ class MarioControllerComponent extends Component {
     if(physics != null){
       physics.setHasFriction(true);
     }
+
+    boundingBox = (BoundingBoxComponent)gameObject.getComponent("BoundingBoxComponent");
   }
 
   void update(float dt) {
     // TODO: fix
     super.update(dt);
+
+    jumpTimer += dt;
+
+    // We want to re-enable the collider, but only after it left the
+    // possibility of touching any other colliders
+    if(jumpTimer > 0.04){
+      boundingBox.setEnableCollisions(true);
+    }
 
     _isInvinsible = Keyboard.isKeyDown(KEY_I);
 
@@ -1700,25 +1741,36 @@ class MarioControllerComponent extends Component {
   }
 
   void idle() {
-    animation.play("idle");
+    if(animation != null){
+      animation.play("idle");
+    }
   }
 
   void walkRight() {
     physics.applyForce(walkForce, 0);
-    animation.setFlipX(false);
-    animation.play("walk");
+
+    if(animation != null){
+      animation.setFlipX(false);
+      animation.play("walk");
+    }
   }
 
   void walkLeft() {
     physics.applyForce(-walkForce, 0);
-    animation.setFlipX(true);
-    animation.play("walk");
+
+    if(animation != null){
+      animation.setFlipX(true);
+      animation.play("walk");
+    }
   }
 
   void jump() {
     // If the player is told to jump but they already are touching
     // a structure at the top, ignore the call.
     BoundingBoxComponent boundingBox = (BoundingBoxComponent)gameObject.getComponent("BoundingBoxComponent");
+    boundingBox.setEnableCollisions(false);
+    jumpTimer = 0.0;
+
     HashMap<String, GameObject> colliders = boundingBox.colliders;
     for(String key : colliders.keySet()){
       GameObject go = colliders.get(key);
@@ -1730,7 +1782,10 @@ class MarioControllerComponent extends Component {
           controller.hit(gameObject);
 
           soundManager.playSound("jump");
-          animation.play("jump");
+
+          if(animation != null){
+            animation.play("jump");
+          }
         }
         return;
       }
@@ -1747,7 +1802,11 @@ class MarioControllerComponent extends Component {
       physics.setTouchingFloor(false);
       physics.applyJumpForce(jumpForce);
       soundManager.playSound("jump");
-      animation.play("jump");
+      
+      if(animation!=null){
+        animation.play("jump");
+      }
+      
       _isJumping = true;
 
       // assume we'll land on the floor.
@@ -1766,7 +1825,7 @@ class MarioControllerComponent extends Component {
 
   // Not moving in either x or y direction
   boolean isIdle() {
-    return  Keyboard.isKeyDown(KEY_LEFT) == false &&
+    return Keyboard.isKeyDown(KEY_LEFT) == false &&
       Keyboard.isKeyDown(KEY_RIGHT) == false &&
       canJump();
   }
@@ -1801,24 +1860,18 @@ class MarioControllerComponent extends Component {
     phy.setTouchingFloor(false);
     phy.velocity.y = 0;
 
-    animation.play("jump");
+    if(animation != null){
+      animation.play("jump");
+    }
   }
 
   void hitStructureY(GameObject structure){
-    // LANDED ON TOP
-    if(gameObject.position.y > structure.position.y){
-      dprintln("Landed on top");
-      _isJumping = false;
-    }
-    // PUNCHED
-    else{
-      if(getJumpState()){
-        dprintln("Punched strucure");
-        physics.setVelocityY(0);
+    if(getJumpState()){
+      dprintln("Punched strucure");
+      physics.setVelocityY(0);
 
-        StructureControllerComponent controller = (StructureControllerComponent)structure.getComponent("StructureControllerComponent");
-        controller.hit(gameObject);
-      }
+      StructureControllerComponent controller = (StructureControllerComponent)structure.getComponent("StructureControllerComponent");
+      controller.hit(gameObject);
     }
   }
 
